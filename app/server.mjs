984d698eea4a1eeea084f54de3178f3722f2ee42
Alexander Bösecke <boesi@users.noreply.github.com>
2024-03-26 21:06:52 +0100

    removed old code

--- app/server.mjs
+++ app/server.mjs
@@ -13,16 +13,4 @@ function start(route, handler) {
 	http.createServer(handleReq).listen(port, () => console.info(`PM-Server running on port ${port}`));
 }
 
-// const server = http.createServer((req, res) => {
-// 	console.log('we have a request');
-// 	res.writeHead(200, {
-// 		'Content-Type': 'text/plain',
-// 	});
-// 	res.end('Hello World');
-// });
-
-// server.listen(port, () => {
-// 	console.log(`Server running at ${port} in directory ${__dirname}`);
-// });
-
 export default start;
