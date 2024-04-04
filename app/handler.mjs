c2f7cca3b3d24b40e031cd67d32fe372af696550
Alexander Bösecke <boesi@users.noreply.github.com>
2024-04-04 21:32:29 +0200

    api defined

--- app/handler.mjs
+++ app/handler.mjs
@@ -1,6 +1,8 @@
 const handler = {
 	'GET /': getNames,
-	'PUT /item': getItem,
+	'GET /item': loadItem,
+	'PUT /item': saveItem,
+	'DELETE /item': deleteItem,
 };
 
 function getNames(res) {
@@ -10,10 +12,23 @@ function getNames(res) {
 	res.end();
 }
 
-function getItem(res, name) {
-	console.info(`Handler getItem with name: ${name}`);
+function loadItem(res, name) {
+	console.info(`test2 Handler loadItem with name: ${name}`);
 	res.writeHead(200, {'Content-Type': 'application/json'});
 	res.write(JSON.stringify({item: {name}}));
+	res.end();
+}
+
+function saveItem(res, item) {
+	console.info(`test2 Handler saveItem with item: ${item}`);
+	res.writeHead(200, {'Content-Type': 'application/json'});
+	res.write(JSON.stringify({item: {item}}));
+	res.end();
+}
+
+function deleteItem(res, name) {
+	console.log(`Handler deleteItem with name: ${name}`);
+	res.writeHead(200);
 	res.end();
 }
 
